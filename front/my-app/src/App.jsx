ee67745cb4076f4f683d4133209cb6d8a1dce475
lukaDoric <luka.doric@uns.ac.rs>
2025-11-12 12:57:57 +0100

    Init.

---
+++ front/my-app/src/App.jsx
@@ -0,0 +1,30 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+
+function App() {
+  const [data, setData] = useState(null);
+
+  // useEffect se izvršava kada se komponenta prvi put prikaže
+  useEffect(() => {
+    axios.get("http://46.62.217.191:44333/WeatherForecast")
+      .then((response) => {
+        setData(response.data); // čuvamo podatke iz API-ja
+      })
+      .catch((error) => {
+        console.error("Greška pri dohvatanju:", error);
+      });
+  }, []); // [] znači da se pokreće samo jednom, na mount
+
+  return (
+    <div>
+      <h1>React + .NET API Test</h1>
+      {data ? (
+        <pre>{JSON.stringify(data, null, 2)}</pre>
+      ) : (
+        <p>Učitavam podatke...</p>
+      )}
+    </div>
+  );
+}
+
+export default App;
